8e1efadd2a7d6b0050016b6fed20e2cc03f5ef18
Vadim Vorobjov <vad.vorobjov@gmail.com>
2021-08-18 19:31:33 +0300

    Replace optional params with enum with success/failure cases

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -52,7 +52,7 @@ class RemoteFeedLoaderTest: XCTestCase {
 		let (sut, client) = makeSUT()
 
 		let samples = [199, 201, 300, 400, 500]
-		
+
 		samples.enumerated().forEach { index, code in
 			var capturedErrors = [RemoteFeedLoader.Error]()
 			sut.load { capturedErrors.append($0) }
@@ -62,7 +62,7 @@ class RemoteFeedLoaderTest: XCTestCase {
 		}
 	}
 
-	// MAKR: - Helpers
+	// MARK: - Helpers
 
 	private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
 		let client = HTTPClientSpy()
@@ -71,26 +71,26 @@ class RemoteFeedLoaderTest: XCTestCase {
 	}
 
 	private class HTTPClientSpy: HTTPClient {
-		private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+		private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 
 		var requestedURLs: [URL] {
 			return messages.map { $0.url }
 		}
 
-		func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+		func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
 			messages.append((url, completion))
 		}
 
 		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(error, nil)
+			messages[index].completion(.failure(error))
 		}
 
 		func complete(withStatusCode code: Int, at index: Int = 0) {
 			let response = HTTPURLResponse(url: requestedURLs[index],
 										   statusCode: code,
 										   httpVersion: nil,
-										   headerFields: nil)
-			messages[index].completion(nil, response)
+										   headerFields: nil)!
+			messages[index].completion(.success(response))
 		}
 	}
 }
