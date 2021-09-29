2346a9297db196614895227194c5c7e08b2570f4
Vadim Vorobjov <vad.vorobjov@gmail.com>
2021-09-29 13:27:37 +0300

    Load command fails on retrieval error

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -19,9 +19,26 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 	func test_load_requestsCacheRetrieval() {
 		let (sut, store) = makeSUT()
 
-		sut.load()
+		sut.load { _ in }
 
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
+	}
+
+	func test_load_fails_onRetreivalError() {
+		let (sut, store) = makeSUT()
+		let retrievalError = anyNSError()
+		let exp = expectation(description: "Wait for load completion")
+		var receivedError: Error?
+
+		sut.load { error in
+			receivedError = error
+			exp.fulfill()
+		}
+
+		store.completeRetrieval(with: retrievalError)
+		wait(for: [exp], timeout: 1.0)
+
+		XCTAssertEqual(receivedError as NSError?, retrievalError)
 	}
 
 	// MAKR: - Helpers
@@ -34,5 +51,9 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		trackForMemoryLeaks(sut)
 
 		return (sut, store)
+	}
+
+	private func anyNSError() -> NSError {
+		return NSError(domain: "any error", code: 0)
 	}
 }
