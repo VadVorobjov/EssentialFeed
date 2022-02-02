af5bbf2bc704ef02222ea1db01c852758d159120
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-02-02 10:07:05 +0200

    Refactor `CachedFeed` type from `enum` to `tuple` since we can represent the absence of a value with an `Optional`

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
-public enum RetrieveCachedFeedResult {
-	case empty
-	case found(feed: [LocalFeedImage], timestamp: Date)
-	case failure(Error)
+public typealias RetrieveCachedFeedResult = Result<CachedFeed, Error>
+
+public enum CachedFeed {
+    case empty
+    case found(feed: [LocalFeedImage], timestamp: Date)
 }
 
 public protocol FeedStore {
