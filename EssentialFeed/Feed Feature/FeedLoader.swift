049d6c3ae19a404550a097b073259f8657f7f5f0
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-05-13 15:44:59 +0300

    Make `MainQueueDispatchDecorator` generic

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -10,5 +10,5 @@ import Foundation
 public protocol FeedLoader {
     typealias Result = Swift.Result<[FeedImage], Error>
 
-    func load(completion: @escaping (FeedLoader.Result) -> Void)
+    func load(completion: @escaping (Result) -> Void)
 }
