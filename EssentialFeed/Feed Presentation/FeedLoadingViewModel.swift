c96820f44c76c34b6700a83f42166811b77bdec2
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-06-05 13:22:05 +0300

    Move `FeedViewModel` to separate file. Move `FeedLoadingViewModel` to separate file. Move `FeedErrorViewModel` to separate file. Replace `FeedPresenter` from iOS module with the `FeedPresenter` from the cross-platform EssentialFeed module

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Vadims Vorobjovs on 05/06/2022.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Vadims Vorobjovs on 05/06/2022.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
