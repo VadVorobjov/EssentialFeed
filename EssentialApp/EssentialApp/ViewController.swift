d72679fed305e8a9c6787a815d7eb39b00693750
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-06-28 21:23:44 +0300

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Vadims Vorobjovs on 28/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
