736e3b0c0b8d348e1afb619220f8e3c301842a6f
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-07-12 19:32:29 +0300

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Vadims Vorobjovs on 12/07/2022.
+//
+
+import XCTest
+
+class EssentialAppUIAcceptanceTests: XCTestCase {
+
+  
+}
