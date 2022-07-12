427a6f14d6bce866507d8d20cc3624d3fd101ae0
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-07-12 19:58:43 +0300

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -9,5 +9,13 @@ import XCTest
 
 class EssentialAppUIAcceptanceTests: XCTestCase {
 
-  
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+//        XCTAssertEqual(app.cells.firstMatch.images.count, 1) fails for some reason
+    }
+    
 }
