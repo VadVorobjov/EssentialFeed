b31a74731a3ebc7d9b1fd490b05b3a2f21c87f05
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-08-12 18:57:18 +0300

    Implement ImageComment mapping

--- EssentialFeed/Image Comments Feature/ImageComment.swift
+++ EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -10,10 +10,10 @@ import Foundation
 public struct ImageComment: Equatable {
     public let id: UUID
     public let message: String
-    public let createdAt: Data
+    public let createdAt: Date
     public let username: String
     
-    public init(id: UUID, message: String, createdAt: Data, username: String) {
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
         self.id = id
         self.message = message
         self.createdAt = createdAt
