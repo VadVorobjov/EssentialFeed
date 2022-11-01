a6cbf03829674870fade39e1ceb17a682c972e9c
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-11-01 20:12:05 +0200

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Vadims Vorobjovs on 01/11/2022.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
