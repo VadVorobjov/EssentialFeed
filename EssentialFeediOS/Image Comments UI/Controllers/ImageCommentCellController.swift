2433923f515f85afc4cb3b30d233887dd79fa30e
Vadim Vorobjov <vad.vorobjov@gmail.com>
2022-11-07 18:08:01 +0200

    Replace protocol composition in a single type with a struct composition to prevent forcing clients to implement methods they don't care about

--- EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -8,7 +8,7 @@
 import EssentialFeed
 import UIKit
 
-public class ImageCommentCellController: NSObject, CellController {
+public class ImageCommentCellController: NSObject, UITableViewDataSource {
     private let model: ImageCommentViewModel
     
     public init(model: ImageCommentViewModel) {
